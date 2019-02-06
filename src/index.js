f9ebbc9bfd59e7f9f39fe1934fa026a5ed0ac635
snake <18501604724@163.com>
2019-02-06 18:48:53 +0800

    lock

---
+++ src/index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const port = 8081;
+console.log("server start....");
+
+const app = express();
+
+app.get("/", (request, response) => {
+  console.log(request.query);
+
+  response.send("Hello, world");
+});
+app.listen(port, () => console.log(`listen ${port} `));
