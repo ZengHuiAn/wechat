175db130e77629127ab177c57fbb2a5f1abd55ac
snake <18501604724@163.com>
2019-02-06 21:30:15 +0800

    崎岖

---
+++ src/confirmWechat.js
@@ -0,0 +1,19 @@
+const sha1 = require("sha1");
+
+const config = {
+  token: "azhwechat"
+};
+export function confirmWeChat(request, response) {
+  const { signature, echostr, timestamp, nonce } = request.query;
+  console.log("request data : \n ", request.query);
+  const { token } = config;
+  const combinedData = [timestamp, nonce, token].sort();
+  const str = combinedData.join("");
+  const encrypt = sha1(str);
+  if (encrypt === signature) {
+    console.log("wechat");
+    response.send(`${echostr}`);
+  } else {
+    console.log("nowechat");
+  }
+}
